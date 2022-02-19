6b0473fc9f748e47a771a7d51d011884053cbf0a
Zhasaral Almabek <thunderccloud@gmail.com>
2022-02-19 15:01:38 +0600

    commited

--- lib/text_field_task.dart
+++ lib/text_field_task.dart
@@ -17,24 +17,27 @@ class _TextFieldTask extends State<TextFieldTask> {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: TextField(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
         
-            controller: _controller,
-            obscureText: true,
-            decoration: InputDecoration(
-              suffixIcon: Icon(Icons.search, color: Colors.purple),
-              hintText: hintText,
-              labelText: 'Search',
-              helperText: 'Поле для поиска заметок',
-              
-              labelStyle: TextStyle(
-                color: Colors.purple
+              controller: _controller,
+              obscureText: true,
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.search, color: Colors.purple),
+                hintText: hintText,
+                labelText: 'Search',
+                helperText: 'Поле для поиска заметок',
+                
+                labelStyle: TextStyle(
+                  color: Colors.purple
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(width: 2, color: Colors.purple),
+                  borderRadius: BorderRadius.circular(10))
               ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(width: 2, color: Colors.purple),
-                borderRadius: BorderRadius.circular(10))
-            ),
 
+            ),
           ),
         )
       )
